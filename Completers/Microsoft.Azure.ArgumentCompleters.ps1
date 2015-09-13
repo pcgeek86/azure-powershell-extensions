d335b9c3b995d8924c45b0427a847b2b1acde7e5
Trevor Sullivan <trevor@trevorsullivan.net>
2015-09-12 20:53:57 -0600

    Update appveyor build and include Patch-ModuleManifest

--- Completers/Microsoft.Azure.ArgumentCompleters.ps1
+++ Completers/Microsoft.Azure.ArgumentCompleters.ps1
@@ -195,20 +195,35 @@ function AzureVirtualMachine_NameCompleter
     return $ItemList
 }
 
-#
-# .SYNOPSIS
-#
-#    Auto-complete the -Location parameter value for Azure cmdlets.
-#
-# .NOTES
-#    
-#    Created by Trevor Sullivan <trevor@trevorsullivan.net>
-#    http://trevorsullivan.net
-#    http://twitter.com/pcgeek86
-#
-function Azure_LocationCompleter
-{
+function Azure_LocationCompleter {
+    <#
+    .SYNOPSIS
+    Auto-complete the -Location parameter value for Azure cmdlets.
+    
+    .NOTES
+    Created by Trevor Sullivan <trevor@trevorsullivan.net>
+    http://trevorsullivan.net
+    http://twitter.com/pcgeek86
+    #>
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    function Get-AllAzureLocations {
+        <#
+        .Synopsis
+        Retrieves a list of Azure locations, using the AzureResourceManager PowerShell module.
+
+        .Description
+
+        This function may be deprecated, or require modification, if the Get-AzureLocation command is changed
+        from returning Azure platform features, to returning Azure locations. As of this implementation, the
+        ARM Get-AzureLocation command returns a list of Azure Resource Providers as the top-level object.
+        #>
+        [CmdletBinding()]
+        param ()
+
+        $LocationList = (AzureResourceManager\Get-AzureLocation).Locations | Select-Object -Unique | Sort-Object;
+        Write-Output -InputObject $LocationList;
+    }
 
     ### Attempt to read Azure virtual machine details from the cache
     $CacheKey = 'Azure_LocationCache';
