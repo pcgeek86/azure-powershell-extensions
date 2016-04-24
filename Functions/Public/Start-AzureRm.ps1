da517f812c27fc3c2d3f0132991ba17e9508eaf6
Trevor Sullivan <trevor@trevorsullivan.net>
2016-04-24 17:43:14 -0600

    Adding Start-AzureRm command to simplify working with Azure in a new PowerShell session.

---
+++ Functions/Public/Start-AzureRm.ps1
@@ -0,0 +1,31 @@
+function Start-AzureRm {
+    <#
+    .Synopsis
+    Loads or creates a cached Azure Profile.
+
+    .Description
+    Loads a cached Azure Profile from disk, if it exists, otherwise the user is prompted to create it.
+    
+    This command has no parameters.
+
+    .Example
+    Start-AzureRm;
+    #>
+    [CmdletBinding()]
+    param ()
+
+    $script:AzureProfilePath = '{0}\AzureProfile.json' -f $env:USERPROFILE;
+    try {
+        ### Load the Azure Profile from the local disk
+        Select-AzureRmProfile -Path $script:AzureProfilePath;
+        Find-AzureRmResourceGroup -Tag @{ Name = 'Company'; };
+    } catch {
+        $AzureProfile = Add-AzureRmAccount -Credential (Get-Credential trevor@trevorsullivan.net);
+        Save-AzureRmProfile -Path $script:AzureProfilePath -Profile $AzureProfile;
+    }
+    
+    ### Load the Azure PowerShell Extensions, if available
+    if (Get-Module -ListAvailable -Name AzureExt) {
+        Import-Module -Name AzureExt;
+    }
+}
