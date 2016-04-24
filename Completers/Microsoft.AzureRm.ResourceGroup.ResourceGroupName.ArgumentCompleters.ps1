7a34dc3b5e19d74073e98ce3f281f164447198c4
Trevor Sullivan <trevor@trevorsullivan.net>
2016-04-23 22:04:28 -0600

    Fixed bug in ResourceGroupName auto-completer

--- Completers/Microsoft.AzureRm.ResourceGroup.ResourceGroupName.ArgumentCompleters.ps1
+++ Completers/Microsoft.AzureRm.ResourceGroup.ResourceGroupName.ArgumentCompleters.ps1
@@ -10,15 +10,15 @@ $ScriptBlock = {
         param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
 
         try {
-            $ResourceGroupList = Get-AzureRmResourceGroup -ErrorAction Stop;
+            $ResourceGroupList = Get-AzureRmResourceGroup -ErrorAction Stop -WarningAction Ignore;
         } catch {
             Write-Host -Object ('Error occurred retrieving resource groups: {0}' -f $PSItem.Exception.Message);
         }
         $ItemList = $ResourceGroupList | Where-Object { $PSItem.ResourceGroupName -match $wordToComplete } | ForEach-Object {
-            CompletionText = $PSItem.ResourceGroupName;
-            ToolTip = 'Resource Group {0} in {1} region.' -f $PSItem.ResourceGroupName, $PSItem.Location;
-            ListItemText = '{0} ({1})' -f $PSItem.ResourceGroupName, $PSItem.Location;
-            CompletionResultType = [System.Management.Automation.CompletionResultType]::ParameterValue;
+            $CompletionText = $PSItem.ResourceGroupName;
+            $ToolTip = 'Resource Group {0} in {1} region.' -f $PSItem.ResourceGroupName, $PSItem.Location;
+            $ListItemText = '{0} ({1})' -f $PSItem.ResourceGroupName, $PSItem.Location;
+            $CompletionResultType = [System.Management.Automation.CompletionResultType]::ParameterValue;
 
             New-Object -TypeName System.Management.Automation.CompletionResult -ArgumentList @($CompletionText, $ListItemText, $CompletionResultType, $ToolTip);
         }
